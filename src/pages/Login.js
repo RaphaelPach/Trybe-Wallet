e6cd3ba6de6e8f9659b4f2aced62a946b8eaa190
Raphael Pacheco <raphaelrogen@gmail.com>
2022-09-30 01:27:45 -0300

    [Requisito 01] - Primeiro Requisito concluído

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,76 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { getEmail } from '../redux/actions';
 
 class Login extends React.Component {
+  state = {
+    inputEmail: '',
+    password: '',
+    isDisabled: true,
+  };
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({ [name]: value }, () => {
+      this.verifyBtn();
+    });
+  };
+
+  verifyBtn = () => {
+    const { inputEmail, password } = this.state;
+    const Regex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i;
+    const verifyEmail = Regex.test(inputEmail);
+    console.log('log', verifyEmail);
+    const number = 6;
+    const verifyPassword = password.length >= number;
+    this.setState({ isDisabled: !(verifyEmail && verifyPassword) });
+  };
+
+  btnSubmit = (e) => {
+    e.preventDefault();
+    const { history, dispatch } = this.props;
+    const { inputEmail } = this.state;
+    dispatch(getEmail(inputEmail));
+    history.push('/carteira');
+  };
+
   render() {
-    return <div>Login</div>;
+    const { isDisabled } = this.state;
+    return (
+      <div>
+        <form>
+          <input
+            name="inputEmail"
+            type="email"
+            onChange={ this.handleChange }
+            data-testid="email-input"
+            required
+          />
+          <input
+            name="password"
+            type="password"
+            onChange={ this.handleChange }
+            data-testid="password-input"
+            required
+          />
+          <button
+            type="submit"
+            onClick={ this.btnSubmit }
+            disabled={ isDisabled }
+          >
+            ENTRAR
+          </button>
+        </form>
+      </div>
+
+    );
   }
 }
 
-export default Login;
+Login.propTypes = {
+  history: PropTypes.objectOf.isRequired,
+  dispatch: PropTypes.func.isRequired,
+};
+
+export default connect()(Login);
