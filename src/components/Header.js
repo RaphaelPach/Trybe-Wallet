c220a365e014e7a49e25102bb0945cba24a01901
Raphael Pacheco <raphaelrogen@gmail.com>
2022-09-30 16:54:46 -0300

    [Requisito 02] - Requisito 02 concluido.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Header extends Component {
   render() {
+    const { email } = this.props;
+    console.log('log', email);
     return (
-      <div>Header</div>
+      <div>
+        Header
+        <p data-testid="email-field">
+          {email}
+        </p>
+        <h2 data-testid="total-field"> 0 </h2>
+        <h3 data-testid="header-currency-field"> BRL </h3>
+      </div>
     );
   }
 }
 
-export default Header;
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+Header.propTypes = {
+  email: PropTypes.func,
+}.isRequired;
+
+export default connect(mapStateToProps)(Header);
